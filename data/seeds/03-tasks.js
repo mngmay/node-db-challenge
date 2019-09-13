c42f2aaaa69186882eb7f48641f04e8b80299137
May Ng <mngmay@gmail.com>
2019-09-13 08:53:04 -0700

    created task seed data

--- data/seeds/03-tasks.js
+++ data/seeds/03-tasks.js
@@ -1,13 +1,53 @@
-
 exports.seed = function(knex) {
-  // Deletes ALL existing entries
-  return knex('table_name').del()
-    .then(function () {
-      // Inserts seed entries
-      return knex('table_name').insert([
-        {id: 1, colName: 'rowValue1'},
-        {id: 2, colName: 'rowValue2'},
-        {id: 3, colName: 'rowValue3'}
-      ]);
-    });
+  return knex("tasks").insert([
+    {
+      id: 1,
+      description: "create GitHub repo",
+      notes: "",
+      completed: true,
+      project_id: 1
+    },
+    {
+      id: 2,
+      description: "create file structure",
+      notes: "organize your plan",
+      completed: true,
+      project_id: 1
+    },
+    {
+      id: 3,
+      description: "styling",
+      notes: "make it pretty",
+      completed: false,
+      project_id: 1
+    },
+    {
+      id: 4,
+      description: "buy supplies",
+      notes: "",
+      completed: false,
+      project_id: 2
+    },
+    {
+      id: 5,
+      description: "make blueprints",
+      notes: "plan the look",
+      completed: false,
+      project_id: 2
+    },
+    {
+      id: 6,
+      description: "secure ripe bananas",
+      notes: "brown spots are good",
+      completed: true,
+      project_id: 3
+    },
+    {
+      id: 7,
+      description: "bake bread",
+      notes: "you should cook it or you'll get a tummy ache",
+      completed: true,
+      project_id: 3
+    }
+  ]);
 };
