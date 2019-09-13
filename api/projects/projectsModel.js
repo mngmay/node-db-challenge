738262c6d52faafc03886dd93a77020a0c0d27db
May Ng <mngmay@gmail.com>
2019-09-13 11:43:51 -0700

    updated to show only columns requested and converted booleans

--- api/projects/projectsModel.js
+++ api/projects/projectsModel.js
@@ -15,10 +15,13 @@ async function findProjectById(id) {
     await db("projects as p")
       .where({ id })
       .first(),
-    await db("tasks as t").where({ project_id: id }),
+    await db("tasks as t")
+      .where({ project_id: id })
+      .select("t.id", "t.description", "t.notes", "t.completed"),
     await db("project_resources as pr")
       .where({ project_id: id })
       .join("resources as r", "r.id", "pr.resource_id")
+      .select("r.id", "r.name", "r.description")
   ];
 }
 
--- api/projects/projectsRouter.js
+++ api/projects/projectsRouter.js
@@ -25,7 +25,12 @@ router.get("/:id", (req, res) => {
     .then(project => {
       project[0].tasks = project[1];
       project[0].resources = project[2];
-      project.completed = Boolean(project.completed);
+      project[0].completed = Boolean(project[0].completed);
+
+      project[0].tasks.forEach(
+        task => (task.completed = Boolean(task.completed))
+      );
+
       return res.status(200).json(project[0]);
     })
     .catch(err => {
