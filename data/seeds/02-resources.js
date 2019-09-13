d316188c17b2ab71966e65120ba6a8f773435d4f
May Ng <mngmay@gmail.com>
2019-09-13 08:49:09 -0700

    created resources seed data

--- data/seeds/02-resources.js
+++ data/seeds/02-resources.js
@@ -1,13 +1,7 @@
-
 exports.seed = function(knex) {
-  // Deletes ALL existing entries
-  return knex('table_name').del()
-    .then(function () {
-      // Inserts seed entries
-      return knex('table_name').insert([
-        {id: 1, colName: 'rowValue1'},
-        {id: 2, colName: 'rowValue2'},
-        {id: 3, colName: 'rowValue3'}
-      ]);
-    });
+  return knex("resources").insert([
+    { id: 1, name: "computer", description: "magic box" },
+    { id: 2, name: "banana", description: "monkeys like them" },
+    { id: 3, name: "wood", description: "how many can a wood chuck chuck?" }
+  ]);
 };
