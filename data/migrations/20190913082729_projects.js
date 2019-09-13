df25c3ebcfffd394716379d334231e2c33d6d3eb
May Ng <mngmay@gmail.com>
2019-09-13 08:33:37 -0700

    created tasks in migration file

--- data/migrations/20190913082729_projects.js
+++ data/migrations/20190913082729_projects.js
@@ -1,19 +1,33 @@
 exports.up = function(knex) {
-  return knex.schema.createTable("projects", tbl => {
-    tbl.increments();
+  return knex.schema
+    .createTable("projects", tbl => {
+      tbl.increments();
 
-    tbl
-      .string("name", 255)
-      .notNullable()
-      .unique();
+      tbl
+        .string("name", 255)
+        .notNullable()
+        .unique();
 
-    tbl.string("description", 255);
+      tbl.string("description", 255);
 
-    tbl
-      .boolean("completed")
-      .defaultTo(false)
-      .notNullable();
-  });
+      tbl
+        .boolean("completed")
+        .notNullable()
+        .defaultTo(false);
+    })
+
+    .createTable("tasks", tbl => {
+      tbl.increments();
+
+      tbl.string("description", 255).notNullable();
+
+      tbl.string("notes", 255);
+
+      tbl
+        .boolean("completed")
+        .notNullable()
+        .defaultTo(false);
+    });
 };
 
 exports.down = function(knex) {};
