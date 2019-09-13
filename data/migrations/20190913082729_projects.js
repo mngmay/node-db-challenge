09e416d8979ba0b557a89398b834e5141d1f8f70
May Ng <mngmay@gmail.com>
2019-09-13 08:38:15 -0700

    created project_resources table in migration file

--- data/migrations/20190913082729_projects.js
+++ data/migrations/20190913082729_projects.js
@@ -44,6 +44,27 @@ exports.up = function(knex) {
         .inTable("projects")
         .onDelete("CASCADE")
         .onUpdate("CASCADE");
+    })
+
+    .createTable("project_resources", tbl => {
+      // FK
+      tbl
+        .integer("project_id")
+        .unsigned()
+        .references("id")
+        .inTable("projects")
+        .onDelete("CASCADE")
+        .onUpdate("CASCADE");
+
+      tbl
+        .integer("resource_id")
+        .unsigned()
+        .references("id")
+        .inTable("resources")
+        .onDelete("CASCADE")
+        .onUpdate("CASCADE");
+
+      tbl.primary(["project_id", "resource_id"]);
     });
 };
 
